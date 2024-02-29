bfb35197400809ea450d8db685fd28879a48e9f8
Chugunov Roman <Zebs-BMK@yandex.ru>
2024-02-29 15:31:47 +0300

    fix: add connection timeout for the usd_rub script

--- scripts/usd_rub.sh
+++ scripts/usd_rub.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 dwm_uasd_rub() {
-  DATA=$(curl -s -H "Accept: application/json" "https://api.tinkoff.ru/v1/currency_rates?from=USD&to=RUB" | awk '{split($0, a, "},{"); for(i in a) { if (a[i] ~ /DebitCardsTransfers/) { n=split(a[i], l, ","); gsub("\"buy\":", "",l[n-1]); gsub("\"sell\":", "",l[n]); print(l[n-1]"/"l[n]); } }}')
+  DATA=$(curl --connect-timeout 3 -s -H "Accept: application/json" "https://api.tinkoff.ru/v1/currency_rates?from=USD&to=RUB" | awk '{split($0, a, "},{"); for(i in a) { if (a[i] ~ /DebitCardsTransfers/) { n=split(a[i], l, ","); gsub("\"buy\":", "",l[n-1]); gsub("\"sell\":", "",l[n]); print(l[n-1]"/"l[n]); } }}')
   echo -e "$DATA"
 }
 
